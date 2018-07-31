b3545043787b8916d679bbd860d884ca833d0714
malli <malli@malli-PC>
2018-07-31 18:45:57 +0530

    1st change file

---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+
+
+public class Employee {
+	private int eid;
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + "]";
+	}
+	
+
+}
